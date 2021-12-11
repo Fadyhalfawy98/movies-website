d99145f63660ccd7fa560a591d5be1a84b1e565e
Fady ElHalfawy <fadyhalfawy@gmail.com>
2021-12-11 14:34:26 +0200

    Update index.tsx to start with movies component.

--- src/index.tsx
+++ src/index.tsx
@@ -2,10 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
+import Movies from "./components/movies";
 
 ReactDOM.render(
   <React.StrictMode>
-    < />
+    <Movies />
   </React.StrictMode>,
   document.getElementById('root')
 );
