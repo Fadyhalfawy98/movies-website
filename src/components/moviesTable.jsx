4ed76887b32cfccef8ef0a440532cdc2cf398efa
Fady ElHalfawy <fadyhalfawy@gmail.com>
2021-12-14 21:05:02 +0200

    Update moviesTable.jsx using displayTable.jsx to make it cleaner.

--- src/components/moviesTable.jsx
+++ src/components/moviesTable.jsx
@@ -1,47 +1,44 @@
 import {Like} from "./like";
-import {TableHeader} from "./tableHeader";
+import {Component} from "react";
+import {DisplayTable} from "./displayTable";
 
-export const MoviesTable = props => {
-    const { filteredMovies, onDelete, handleLike } = props;
+export default class MoviesTable extends Component {
+    render() {
+        const { filteredMovies, onDelete, handleLike, onSort, sortColumn } = this.props;
 
-    const columns = [
-        { path: "title", label: "Title"},
-        { path: "genre", label: "Genre"},
-        { path: "numberInStock", label: "Stock"},
-        { path: "dailyRentalRate", label: "Rate"}
-        ]
+        const columns = [
+            { path: "title", label: "Title"},
+            { path: "genre.name", label: "Genre"},
+            { path: "numberInStock", label: "Stock"},
+            { path: "dailyRentalRate", label: "Rate"},
 
-    return(
-        <table className="table table-dark">
-
-            <TableHeader
-                columns={columns}
-            />
-
-            <tbody>
-            {filteredMovies.map((movie, index) => (
-                <tr key={movie._id}>
-                    <td>{index + 1}</td>
-                    <td>{movie.title}</td>
-                    <td>{movie.genre.name}</td>
-                    <td>{movie.numberInStock}</td>
-                    <td>{movie.dailyRentalRate}</td>
-
-                    <td>{<Like
+            { key: "liked",
+                content: movie => (
+                    <Like
                         onLike={ handleLike }
                         movie={ movie }
-                    />}</td>
+                    />
+                )},
 
-                    <td><button
+            { key: "delete",
+                content: movie => (
+                    <button
+                        type="button"
+                        className="btn btn-outline-danger btn-sm"
                         onClick={() => onDelete(movie)}
-                        className={"btn btn-outline-danger btn-sm"}>
+                    >
                         Delete
-                    </button></td>
-                </tr>
-            ))}
+                    </button>
+                )}
+        ]
 
-            </tbody>
-
-        </table>
-    );
+        return(
+            <DisplayTable
+                filteredMovies={filteredMovies}
+                columns={columns}
+                onSort={onSort}
+                sortColumn={sortColumn}
+            />
+        );
+    };
 }
