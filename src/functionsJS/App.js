8b5889e0010b08a4c604b10ea7012c508c4db159
Fady ElHalfawy <fadyhalfawy@gmail.com>
2021-12-17 11:47:23 +0200

    Update App.js.

--- src/functionsJS/App.js
+++ src/functionsJS/App.js
@@ -2,14 +2,14 @@ import React from "react";
 import {Component} from "react";
 import Movies from "../components/movies";
 import Route from "react-router-dom/Route";
-import Customers from "../components/customers";
-import Rentals from "../components/rentals";
+import CustomersForm from "../components/forms/customersForm";
+import RentalsForm from "../components/forms/rentalsForm";
 import {Redirect, Switch} from "react-router-dom";
 import NavBar from "../components/navBar";
-import MoviesForm from "../components/moviesForm";
-import ErrorFound from "../components/errorFound";
-import LoginForm from "../components/loginForm";
-import SignupForm from "../components/signupForm";
+import MoviesForm from "../components/forms/moviesForm";
+import NotFoundForm from "../components/forms/notFoundForm";
+import LoginForm from "../components/forms/loginForm";
+import SignupForm from "../components/forms/signupForm";
 
 export class App extends Component {
     render() {
@@ -21,13 +21,13 @@ export class App extends Component {
             <main className={"container"}>
 
                 <Switch>
+                    <Route path={"/notfound"} to={NotFoundForm} />
                     <Route path={"/movies/:id/:title?"} component={MoviesForm} />
                     <Route path={"/movies"} component={Movies} />
                     <Route path={"/login"} component={LoginForm} />
                     <Route path={"/signup"} component={SignupForm} />
-                    <Route path={"/customers"} component={Customers} />
-                    <Route path={"/rentals"} component={Rentals} />
-                    <Route path={"/notfound"} to={ErrorFound} />
+                    <Route path={"/customers"} component={CustomersForm} />
+                    <Route path={"/rentals"} component={RentalsForm} />
                     <Redirect from={"/"} exact to={"/movies"} />
                     <Redirect to={"/notfound"} />
                 </Switch>
