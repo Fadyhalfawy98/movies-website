ebdaecb0915c35d92808a3a075bcc79ccf872d13
Fady ElHalfawy <fadyhalfawy@gmail.com>
2021-12-18 21:49:23 +0200

    Add selectForm.jsx.

---
+++ src/components/forms/selectForm.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const SelectForm = ({ id, label, options, error, ...rest }) => {
+    return(
+        <div className="form-group">
+            <label className="form-choose-label"
+                   htmlFor={ id }>{label}</label>
+
+            <select className="custom-select" name={id} id={id} {...rest} required>
+                <option value="" />
+                {options.map(option => (
+                    <option key={option._id} value={option._id}>
+                        {option.name}
+                    </option>
+                ))}
+            </select>
+        </div>
+    );
+}
+
+export default SelectForm;
