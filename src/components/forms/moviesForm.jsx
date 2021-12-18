4db829d8771e196f328587218383a40581240b13
Fady ElHalfawy <fadyhalfawy@gmail.com>
2021-12-18 20:11:31 +0200

    Update moviesForm.jsx.

--- src/components/forms/moviesForm.jsx
+++ src/components/forms/moviesForm.jsx
@@ -1,21 +1,19 @@
 import React from "react";
-import MainForm from "./mainForm";
+import HandleButtonTransfer from "../../helperFunctions/handleButtonTransfer";
+const MoviesForm = ({ match, history }) => {
+  return(
+      <div>
+        <h1> Movie id = {match.params.id} and name is = { match.params.title } </h1>
+          
+        <button
+            className={"btn btn-outline-info my-2 my-sm-0"}
+            onClick={() => HandleButtonTransfer(history, "/movies")}>
+          Save
+        </button>
 
-class MoviesForm extends MainForm {
-    render() {
-        const {match, history} = this.props;
-        return(
-            <React.Fragment>
+      </div>
 
-                <h1> Movie id = {match.params.id} and name is = { match.params.title } </h1>
-
-                {this.renderButton("btn-outline-info", "Save", false, history, "/movies")}
-
-            </React.Fragment>
-
-        );
-}
-
+  );
 }
 
 export default MoviesForm;
