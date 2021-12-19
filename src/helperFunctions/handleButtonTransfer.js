67e211e7a780f6b271c6fb8d399130969b67b5c7
Fady ElHalfawy <fadyhalfawy@gmail.com>
2021-12-19 07:56:57 +0200

    Update handleButtonTransfer.js by adding a condition to save movies.

--- src/helperFunctions/handleButtonTransfer.js
+++ src/helperFunctions/handleButtonTransfer.js
@@ -1,6 +1,14 @@
-const HandleButtonTransfer = (history, path, label)  => {
+import {saveMovie} from "../services/fakeMovieService";
+
+const HandleButtonTransfer = (history, path, label, movie)  => {
 
   if (label === "Signup") return history.replace(path);
+
+
+  if (label === "Save") {
+    saveMovie(movie);
+    return history.replace(path);
+  }
 
   return history.push(path);
 }
