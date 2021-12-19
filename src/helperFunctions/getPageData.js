50a87b3b80ec7d10da07a980a51ab2a15360eabf
Fady ElHalfawy <fadyhalfawy@gmail.com>
2021-12-19 05:51:23 +0200

    Update getPageData.js by adding searchQuery.

--- src/helperFunctions/getPageData.js
+++ src/helperFunctions/getPageData.js
@@ -2,9 +2,10 @@ import {handleFilteredMovie} from "./handleSelectedGenre";
 import _ from "lodash";
 import {paginate} from "./paginate";
 
-export const getPageData = (allMovies, selectedGenre, pageSize, currentPage, sortColumn) => {
+export const getPageData = (allMovies, selectedGenre, pageSize, currentPage, sortColumn, searchQuery) => {
+    let filtered = allMovies;
 
-    const moviesFiltered = handleFilteredMovie(allMovies, selectedGenre);
+    const moviesFiltered = handleFilteredMovie(filtered, selectedGenre, searchQuery);
 
     const moviesSorted = _.orderBy(moviesFiltered, [sortColumn.path], [sortColumn.order]);
 
