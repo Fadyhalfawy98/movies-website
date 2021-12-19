2123ddac2dc4a225bf83202a3c6c46ef2e89a751
Fady ElHalfawy <fadyhalfawy@gmail.com>
2021-12-19 05:48:04 +0200

    Update handleSelectedGenre.js by adding else if for searchQuery.

--- src/helperFunctions/handleSelectedGenre.js
+++ src/helperFunctions/handleSelectedGenre.js
@@ -1,11 +1,14 @@
 import {genres} from "../services/fakeGenreService";
 
-export let handleFilteredMovie = (allMovies, selectedGenre) => {
+export let handleFilteredMovie = (allMovies, selectedGenre, searchQuery) => {
     let movies;
-    if(selectedGenre === genres[0])
+    if(selectedGenre === genres[0] || searchQuery === "")
         movies = allMovies;
     else if(selectedGenre === genres[4])
         movies = allMovies.filter(movie => true === movie.liked);
+    else if (searchQuery){
+        movies = allMovies.filter(m => m.title.toLowerCase().startsWith(searchQuery.toLowerCase()));
+    }
     else
         movies = allMovies.filter(movie => selectedGenre._id === movie.genre._id);
 
