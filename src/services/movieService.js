11786cff40f7f272516f601a7657c1d5122cead1
Fady ElHalfawy <fadyhalfawy@gmail.com>
2021-12-23 02:38:25 +0200

    Update movieServices.js by adding getMovie function and saveMovie Function.

--- src/services/movieService.js
+++ src/services/movieService.js
@@ -5,6 +5,19 @@ export function getMovies() {
     return http.get(config.moviePath);
 
 };
+export function getMovie(moveId) {
+    return http.get(config.moviePath + "/" + moveId);
+};
+
+export function saveMovie(movie) {
+    if (movie._id){
+        const body = { ...movie };
+        delete body._id;
+        return http.put(config.moviePath + "/" + movie._id, body);
+    }
+
+    return http.post(config.moviePath, movie);
+};
 
 export function deleteMovie(movieId) {
     return http.delete(config.moviePath + "/" + movieId);
