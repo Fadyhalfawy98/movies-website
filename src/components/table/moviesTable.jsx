81b736a247ceb46f99a285edcab051b9e03c35e3
Fady ElHalfawy <fadyhalfawy@gmail.com>
2021-12-26 06:37:11 +0200

    Update movieTable.jsx.

--- src/components/table/moviesTable.jsx
+++ src/components/table/moviesTable.jsx
@@ -2,49 +2,60 @@ import {Like} from "../common/like";
 import {Component} from "react";
 import {DisplayTable} from "./displayTable";
 import {Link} from "react-router-dom";
+import auth from "../../services/authService";
 
 export default class MoviesTable extends Component {
+
+    constructor(props) {
+        super(props);
+        const user = auth.getCurrentUser();
+        if (user && user.isAdmin) this.columns.push(this.deleteColumn);
+    }
+
     render() {
-        const { filteredMovies, onDelete, handleLike, onSort, sortColumn } = this.props;
-
-        const columns = [
-            { path: "title", label: "Title",
-                content: movie => (
-                    <Link to={`/movies/${movie._id}/${movie.title}`}>
-                        {movie.title}
-                    </Link>
-                )},
-            { path: "genre.name", label: "Genre"},
-            { path: "numberInStock", label: "Stock"},
-            { path: "dailyRentalRate", label: "Rate"},
-
-            { key: "liked",
-                content: movie => (
-                    <Like
-                        onLike={ handleLike }
-                        movie={ movie }
-                    />
-                )},
-
-            { key: "delete",
-                content: movie => (
-                    <button
-                        type="button"
-                        className="btn btn-outline-danger btn-sm"
-                        onClick={() => onDelete(movie)}
-                    >
-                        Delete
-                    </button>
-                )}
-        ]
+        const { filteredMovies, onSort, sortColumn } = this.props;
 
         return(
             <DisplayTable
                 filteredMovies={filteredMovies}
-                columns={columns}
+                columns={this.columns}
                 onSort={onSort}
                 sortColumn={sortColumn}
             />
         );
     };
+
+    columns = [
+        { path: "title", label: "Title",
+            content: movie => (
+                <Link to={`/movies/${movie._id}/${movie.title}`}>
+                    {movie.title}
+                </Link>
+            )},
+        { path: "genre.name", label: "Genre"},
+        { path: "numberInStock", label: "Stock"},
+        { path: "dailyRentalRate", label: "Rate"},
+        { key: "liked",
+            content: movie => (
+                <Like
+                    onLike={ this.props.handleLike }
+                    movie={ movie }
+                />
+            )
+        }
+    ];
+
+    deleteColumn = {
+        key: "delete",
+        content: movie => (
+            <button
+                type="button"
+                className="btn btn-outline-danger btn-sm"
+                onClick={() => this.props.onDelete(movie)}
+            >
+                Delete
+            </button>
+        )
+    };
+
 }
